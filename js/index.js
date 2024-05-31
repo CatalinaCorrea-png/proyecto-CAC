a5978dd0cac6acb8ffe97147e13bf7f636606d79
unknown <rocioj433@gmail.com>
2024-05-31 12:26:48 -0300

    cambios para el consumo de la API

---
+++ js/index.js
@@ -0,0 +1,36 @@
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5Y2NjNjhlM2RhMmIyMjFhZGQwYWIxNmZkYzBlZWRmYyIsInN1YiI6IjY2NThhN2ZjOWNkNDFiMjg3YTkzZTZjOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Pfa7G9p7yRGM1ECjPDxox_zPd-TsQu9lElR6lSTgYno",
+  },
+};
+
+const BASE_URL = "https://api.themoviedb.org/3";
+
+let currentPage = 1;
+
+function llamarAPI(page) {
+  fetch(`${BASE_URL}/movie/top_rated?language=en-US&page=${page}`, options)
+    .then((response) => response.json())
+    .then((data) => dibujaDatos(data));
+}
+
+llamarAPI(1);
+
+function dibujaDatos(json){
+  const filas = json.results.map(obj => Pelicula(obj));
+
+  document.querySelector('.aclamadas .aclamadasContainer').innerHTML = filas.join('');
+}
+
+function Pelicula(obj) {
+  return `
+    <div class="aclamadaItem">
+    <img src="https://image.tmdb.org/t/p/w500/${obj.poster_path}" class="imagen" alt="${obj.title}">
+    
+    <p>${obj.title}</p>
+    </div>
+    `;
+}
